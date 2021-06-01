43213d5490145b2fe1c1389dc8f97ef258d728f2
BeggiNN <49918901+BeggiNN@users.noreply.github.com>
2021-06-01 15:21:34 +0300

    Add files via upload

--- SalesEndTime/Block/Index.php
+++ SalesEndTime/Block/Index.php
@@ -18,6 +18,11 @@ class Index extends \Magento\Framework\View\Element\Template
     protected $_configurable;
 
     /**
+     * @var \Magento\CatalogRule\Model\Rule
+     */
+    protected $_rules;
+
+    /**
      * @var \Magento\Catalog\Block\Product\View\AbstractView
      */
     protected $_view;
@@ -28,8 +33,13 @@ class Index extends \Magento\Framework\View\Element\Template
         Registry $registry,
         Configurable $configurable,
         \Magento\Catalog\Block\Product\View\AbstractView $view,
+        \Magento\CatalogRule\Model\Rule $rules,
+        \Magento\Framework\Stdlib\DateTime\DateTime $dateHelper,
         array $data = []
-    ) {
+    )
+    {
+        $this->_rules = $rules;
+        $this->_dateHelper = $dateHelper;
         $this->_view = $view;
         $this->_productRepository = $productRepository;
         $this->registry = $registry;
@@ -38,36 +48,110 @@ class Index extends \Magento\Framework\View\Element\Template
     }
 
     /**
-     * Get date end special price.
+     * Get end date special price
      * @return array|string
      */
     public function getDatePrice()
     {
+        $text = "Special Price";
         $product = $this->_view->getProduct();
         if (isset($product)) {
             $type = $product->getTypeId();
-            if ($type == 'configurable'){
+            if ($type == 'configurable') {
                 $id = $product->getId();
                 $products = $this->_configurable->getChildrenIds($id);
-            foreach ($products[0] as $value) {
-                $item = $this->_productRepository->getById($value);
-                $specialPrice = $item->getSpecialPrice();
-                if (isset($specialPrice)) {
-                    $endTimeSale = $item->getSpecialToDate();
-                    $startTimeSale = $item->getSpecialFromDate();
-                    $endConvert = substr(str_replace("-", "/", $endTimeSale), 0, 10);
-                    return [$endConvert, $startTimeSale,$endTimeSale];
+                foreach ($products[0] as $value) {
+                    $item = $this->_productRepository->getById($value);
+                    $specialPrice = $item->getSpecialPrice();
+                    if (isset($specialPrice)) {
+                        $endTimeSale = $item->getSpecialToDate();
+                        $startTimeSale = $item->getSpecialFromDate();
+                        $nowDate = date('Y-m-d h:m:s');
+                        if ($startTimeSale <= $nowDate && $nowDate <= $endTimeSale) {
+                            return [$endTimeSale, $text];
+                        } else
+                            return '';
+                    }
                 }
-                }
-            return '';
-            }else{
+                return '';
+            } else {
                 $price = $product->getSpecialPrice();
                 if (isset($price)) {
                     $endTimeSale = $product->getSpecialToDate();
                     $startTimeSale = $product->getSpecialFromDate();
-                    $endConvert = substr(str_replace("-", "/", $endTimeSale), 0, 10);
-                    return [$endConvert, $startTimeSale,$endTimeSale];
+                    $nowDate = date('Y-m-d h:m:s');
+                    if ($startTimeSale <= $nowDate && $nowDate <= $endTimeSale) {
+                        return [$endTimeSale, $text];
+                    }
                 }
+            }
+            return '';
+        }
+        return '';
+    }
+
+    /**
+     * Get currect product rules and end date rule.
+     * @return array|string
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getCurrectProductRules()
+    {
+        $text = "Rule Price";
+        $productId = $this->_view->getProduct()->getId();
+        $rules = $this->_rules->getResourceCollection()->addFieldToFilter('is_active', 1);
+        if (!empty($rules)) {
+            foreach ($rules as $rule) {
+                if (isset($rule->getMatchingProductIds()[$productId][1])) {
+                    $productsRules[] = $rule;
+                }
+            }
+        }
+        if (isset($productsRules)) {
+            reset($productsRules);
+            for ($i = 0; $i < count($productsRules) - 1; ++$i) {
+                $current = current($productsRules);
+                $next = next($productsRules);
+                if ($current->getToDate() >= $next->getToDate()) {
+                    $dateTo = $next->getToDate();
+                } else {
+                    $dateTo = $current->getToDate();
+                }
+                return [$dateTo, $text];
+            }
+        }
+        if (isset($productsRules)) {
+            foreach ($productsRules as $rulesa) {
+                $dateTo = $rulesa->getToDate();
+            }
+            return [$dateTo, $text];
+        }
+        return '';
+    }
+
+    /**
+     * Get All catalog rules
+     *
+     * @return array|string
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getCatalogRulePrices()
+    {
+        $productId = $this->_view->getProduct()->getId();
+        $rules = $this->_rules->getResourceCollection()->addFieldToFilter('is_active', 1);
+        if (!empty($rules)) {
+            foreach ($rules as $rule) {
+                if (isset($rule->getMatchingProductIds()[$productId][1])) {
+                    $productsRules[] = $rule;
+                }
+            }
+            if (isset($productsRules)) {
+                foreach ($productsRules as $rules) {
+                    $name = $rules->getName();
+                    $date = $rules->getToDate();
+                    $arr[] = ['name' => $name, 'date' => $date];
+                }
+                return $arr;
             }
         }
         return '';
